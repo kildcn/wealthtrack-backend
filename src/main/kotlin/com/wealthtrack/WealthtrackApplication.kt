1026f3aa52c6e0ceba5a542469b119a102970dea
Killian Le Doucen <killian.ledoucen@yahoo.fr>
2025-04-09 16:47:50 +0200

    Fix class name casing and add BadRequestException for error handling

--- src/main/kotlin/com/wealthtrack/WealthtrackApplication.kt
+++ src/main/kotlin/com/wealthtrack/WealthtrackApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class wealthtrackApplication
+class WealthtrackApplication
 
 fun main(args: Array<String>) {
-    runApplication<wealthtrackApplication>(*args)
+    runApplication<WealthtrackApplication>(*args)
 }
